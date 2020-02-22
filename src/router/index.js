88eed352b65859766247217f635a2c7d6646714a
xxj435 <861479614@qq.com>
2020-02-22 13:50:32 +0800

    路由整合,项目开始

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+//导入路由
+import VueRouter from 'vue-router'
+//注册路由
+Vue.use(VueRouter)
+//实例化
+const router =new VueRouter({
+    routes:[]
+})
+//暴露出去
+export default router
